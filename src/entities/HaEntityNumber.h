0c3dcf509452eef42d2d222c0815a11ae0f97c46
Brishen Hawkins <brishen.hawkins@gmail.com>
2025-06-06 21:21:33 -0400

    chore: Set default templates for number entity

--- src/entities/HaEntityNumber.h
+++ src/entities/HaEntityNumber.h
@@ -51,12 +51,12 @@ public:
     /**
      * @brief Defines a template to generate the payload to send to command_topic.
      */
-    std::string command_template = "";
+    std::string command_template = "{{ value }}";
 
     /**
      * @brief Defines a template to extract the value from the payload received on state_topic.
      */
-    std::string value_template = "";
+    std::string value_template = "{{ value }}";
 
     /**
      * @brief Defines the resolution of the values, i.e. the step.
